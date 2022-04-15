31f0e7393b2150fa2a41a50df99dbc3a79fe89aa
sofia <sofia.pinuelam@autonoma.edu.co>
2022-04-15 16:24:04 -0500

    Modificación al Home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,16 @@
 <div class="container d-flex justify-content-center">
     <div>
     <div>
-        <img class="center" src="" width="400" height="400">
+        <img class="center" src="https://i.pinimg.com/originals/1a/63/4c/1a634c21b9cb6d655fe957998b3c7f3c.jpg" width="780" height="300">
     </div>
-
         <div class="d-flex justify-content-around">
 
-    </div>
+            <a class="btn btn-primary" href="#" role="button">Arrendar</a>
+            <a class="btn btn-primary" href="#" role="button">Publicar</a>
+            <a class="btn btn-primary" href="#" role="button">Sobre Nosotros</a>
+
+        </div>
+        
     </div>
 </div>
 @endsection
