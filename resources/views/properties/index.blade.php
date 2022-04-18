386815273e61b19aa05bcd62c82f46e33a882c38
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-04-18 14:52:16 -0500

    modificacion de vista

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -5,9 +5,10 @@
         <h1 class="text-center">Inmueble</h1>
        @include('layouts.subview-form-errors')
 
-     {!! Form::model($property, ['method' => 'post', 'route' => ['properties.store', $property->id]]) !!}
-        @include('properties.subview-property')
-     {!! Form::close() !!}
+        {!! Form::open(['route' => 'properties.store', 'method' => 'property']) !!}
+       @include('properties.subview-property')
+        {!! Form::close() !!}
+
 
      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15897.81167237157!2d-75.46941715!3d5.03005245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1650291659549!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
