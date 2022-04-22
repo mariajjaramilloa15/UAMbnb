a05d9d016827e19ee44ce2a4bfc3292466cb24d4
Isabella <isabella.grajalesl@autonoma.edu.co>
2022-04-22 15:37:14 -0500

    Se modificó formulario create para almacenamiento de imagen

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -1,7 +1,8 @@
 @extends("layouts.app")
 @section("content")
 
-<div class="centrar1">
+<div class='container'>
+
     <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Nombre</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
@@ -14,10 +15,10 @@
         <label for="exampleFormControlInput1" class="form-label">Email address</label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
       </div>
-      
+
 
 
 <a class="btn btn-primary" href="{{ url('https://www.pse.com.co/') }}" role="button">Pagar</a>
-</div>
 
+</div>
 @endsection
