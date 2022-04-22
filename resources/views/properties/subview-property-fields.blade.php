a6146630cd4bc76b7296074b95ee917ea1a1792d
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-04-22 14:14:07 -0500

    Se agregaron campos en la subvista de Inmuebles, en la parte de editar

--- resources/views/properties/subview-property-fields.blade.php
+++ resources/views/properties/subview-property-fields.blade.php
@@ -1,5 +1,11 @@
 <div class="mb-3">
-    {!! Form::text('name', null, ['class' => 'form-control', 'rows' => 3]); !!}
-    {!! Form::text('descripcion', null, ['class' => 'form-control', 'rows' => 3]); !!}
-    {!! Form::Number('precio', null, ['class' => 'form-control', 'rows' => 3]); !!}
+   <h5>Nombre</h5> {!! Form::text('name', null, ['class' => 'form-control', 'rows' => 3]); !!}
+   <br>
+   <h5>Descripción</h5> {!! Form::text('descripcion', null, ['class' => 'form-control', 'rows' => 3]); !!}
+   <br>
+   <h5>Área</h5> {!! Form::Number('area', null, ['class' => 'form-control', 'rows' => 3]); !!}
+   <br>
+   <h5>Precio</h5> {!! Form::Number('precio', null, ['class' => 'form-control', 'rows' => 3]); !!}
+   <br>
+   <h5>Caracteristicas</h5> {!! Form::text('caracteristicas', null, ['class' => 'form-control', 'rows' => 3]); !!}
 </div>
