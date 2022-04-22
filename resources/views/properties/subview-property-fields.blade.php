870828c3b956c488d8e392ce0109568dbd6f82b9
Isabella <isabella.grajalesl@autonoma.edu.co>
2022-04-22 12:13:59 -0500

    se modificó el CRUD de pripiedades

--- resources/views/properties/subview-property-fields.blade.php
+++ resources/views/properties/subview-property-fields.blade.php
@@ -1,5 +1,5 @@
 <div class="mb-3">
     {!! Form::text('name', null, ['class' => 'form-control', 'rows' => 3]); !!}
-    {!! Form::text('descripcion', null, ['class' => 'form-control', 'rows' => <3]); !!}
+    {!! Form::text('descripcion', null, ['class' => 'form-control', 'rows' => 3]); !!}
     {!! Form::number('precio', null, ['class' => 'form-control', 'rows' => 3]); !!}
 </div>
