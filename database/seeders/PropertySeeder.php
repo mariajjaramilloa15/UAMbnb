95ba68a22a9021bec6da27c38c4783b968e1a0b3
sofia <sofia.pinuelam@autonoma.edu.co>
2022-04-26 11:13:35 -0500

    Modificación de datos

--- database/seeders/PropertySeeder.php
+++ database/seeders/PropertySeeder.php
@@ -16,53 +16,61 @@ class PropertySeeder extends Seeder
     public function run()
     {
         Property::create([
-            'name' => 'Apartamento',
-            'descripcion' => 'fsdf',
-            'area' => 12,
-            'precio' => 12000,
-            'caracteristicas' => 'fafd',
+            'name' => 'Apartamento en Arriendo Manizales',
+            'descripcion' => 'Iluminado apartamento, segundo piso, ascensor, vista exterior, sala-comedor, dos alcobas, estudio, garaje cubierto independiente, conjunto parque infantil, salón comunal, cerca a centro comercial.',
+            'area' => 79,
+            'precio' => 12000000,
+            'caracteristicas' => 'Calentador a gas, Cocina Integral, Garaje Independiente, Iluminación natural,
+            Sala-Comedor, Vista exterior, Zona de ropas, Ascensor, En conjunto cerrado, Garaje Cubierto,
+            Portería/Vigilancia, Salón Comunal, Zona Infantil, Zona Comercial y universitaria, Zona Residencial',
             'Lat' => 1000,
             'Lng' => 2000,
-            'comentarios' => '5',
+            'comentarios' => 'Esta muy bien ubicado, es muy bonito!',
             'likes' => 12,
             'user_id' => 1
         ]);
 
         Property::create([
-            'name' => 'Apartamento',
-            'descripcion' => 'ddawd',
-            'area' => 12,
-            'precio' => 12000,
-            'caracteristicas' => 'dsd',
+            'name' => 'Apartaestudio en Arriendo Manizales ',
+            'descripcion' => 'Iluminado apartaestudio, tercer piso, ascensor, vista exterior, sala-comedor, 1 alcoba, estudio, garaje cubierto independiente, salón comunal, cerca a la universidad de Manizales.',
+            'area' => 55,
+            'precio' => 1150000,
+            'caracteristicas' => 'Calentador a gas, Garaje Independiente, Iluminación natural,
+            Sala-Comedor, Vista exterior, Zona de ropas, Ascensor, En conjunto cerrado, Garaje Cubierto,
+            Portería/Vigilancia, Salón Comunal, Zona universitaria, Zona Residencial',
             'Lat' => 5000,
             'Lng' => 3000,
-            'comentarios' => '4',
+            'comentarios' => 'Es ideal para un estudiante universitario, comodo y lindo.',
             'likes' => 10,
             'user_id' => 2
         ]);
 
         Property::create([
-            'name' => 'dsaf',
-            'descripcion' => 'dwfe',
+            'name' => 'Apartaestudio en Arriendo Manizales',
+            'descripcion' => 'Iluminado apartaestudio, Quinto piso, ascensor, vista exterior, sala-comedor, 1 alcoba, estudio, garaje cubierto independiente, salón comunal, cerca a la universidad Autonoma.',
             'area' => 12,
-            'precio' => 12000,
-            'caracteristicas' => 'djsand',
+            'precio' => 1500000,
+            'caracteristicas' => 'Calentador a gas, Cocina Integral, Garaje Independiente, Iluminación natural,
+            Sala-Comedor, Vista exterior, Zona de ropas, Ascensor, En conjunto cerrado, Garaje Cubierto,
+            Portería/Vigilancia, Salón Comunal, Zona universitaria, Zona Residencial',
             'Lat' => 7000,
             'Lng' => 9000,
-            'comentarios' => '2',
+            'comentarios' => 'Es muy bonito, comodo e ideal para estudiantes',
             'likes' => 18,
             'user_id' => 3
         ]);
 
         Property::create([
-            'name' => 'adad',
-            'descripcion' => 'holaaaaaa',
-            'area' => 12,
-            'precio' => 12000,
-            'caracteristicas' => 'aahjdhs',
+            'name' => 'Apartamento en Arriendo en Manizales',
+            'descripcion' => 'Iluminado apartamento en Palermo, cuarto piso, ascensor, vista exterior, sala-comedor, tres alcobas, estudio, garaje cubierto independiente, conjunto parque infantil, salón comunal, cerca a centro comercial Sancancio.',
+            'area' => 89,
+            'precio' => 2300000,
+            'caracteristicas' => 'Calentador a gas, Cocina Integral, Garaje Independiente, Iluminación natural,
+            Sala-Comedor, Vista exterior, Zona de ropas, Ascensor, En conjunto cerrado, Garaje Cubierto,
+            Portería/Vigilancia, Salón Comunal, Zona Infantil, Zona Comercial, Zona Residencial',
             'Lat' => 6000,
             'Lng' => 2000,
-            'comentarios' => '2',
+            'comentarios' => 'Apartamento grande y bonito, bien ubicado',
             'likes' => 16,
             'user_id' => 4
         ]);
