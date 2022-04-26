e72d520d18b30fad8140f34c44c658828b85273d
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-04-26 11:38:58 -0500

    Implementación del mapa en las vistas

--- resources/views/properties/property.blade.php
+++ resources/views/properties/property.blade.php
@@ -49,8 +49,31 @@
     </p>
   </form>
 
+  <script type="text/javascript">
+    var myCenter=new google.maps.LatLng(39.46910009461806, -0.39186259999996764);
+
+    var marker = new google.maps.Marker({
+            position: myCenter
+            ,});
+
+    window.onload = function() {
+            var mapOptions = {
+              center: myCenter,
+              zoom: 16,
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              panControl: false,
+              zoomControl: false,
+              scaleControl: false,
+              };
+            var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        marker.setMap(map);
+            };
+    </script>
+
+    <div id="map_canvas"></div>
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15897.81167237157!2d-75.46941715!3d5.03005245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1650291659549!5m2!1ses!2sco" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
 <div>
 <br>
 <br>
